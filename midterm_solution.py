81725718258ec492edfaa0fd50f9912a67e08e7e
Price <shimadagenji57@gmail.com>
2026-04-18 18:36:24 +0800

    Added Budget Input Code Block

--- midterm_solution.py
+++ midterm_solution.py
@@ -16,3 +16,27 @@ while True:
         else:
             print("Invalid input. No spaces allowed.")
 
+# ------------------ BUDGET INPUT ------------------
+while True:
+    budget_input = input("Enter weekly budget: ")
+
+    valid = True
+
+    if budget_input == "":
+        valid = False
+    else:
+        for ch in budget_input:
+            if ch < "0" or ch > "9":
+                valid = False
+
+    if valid == True:
+        weekly_budget = int(budget_input)
+        break
+    else:
+        print("Invalid input. Please enter a number.")
+
+print("")
+print("Student name:", name)
+print("Weekly budget:", weekly_budget)
+print("")
+
